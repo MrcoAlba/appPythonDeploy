ed78503368a7cb6d07dbad10eb4d7b83565e4f59
MrcoAlba <mrcoalba@gmail.com>
2024-05-28 04:24:40 -0500

    final

--- test_ops.py
+++ test_ops.py
@@ -1,13 +1,13 @@
 from ops import *
 
 def test_add():
-    assert add(3,3) == 6
+    assert opsadd(3,3) == 6
 
 def test_subtract():
-    assert subtract(2, 3) == -1
+    assert opssubtract(2, 3) == -1
 
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert opsmultiply(2, 3) == 6
 
 def test_divide():
-    assert divide(10,5) == 2
+    assert opsdivide(10,5) == 2
